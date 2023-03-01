2368d54f09ab8141d847a6c0c896d75dae8c6951
joselu <joselu201@corre.ugr.es>
2023-03-01 12:54:01 +0100

    S1

---
+++ S1/hola7.asm
@@ -0,0 +1,29 @@
+pila segment stack 'stack'
+	dw 100h dup (?)
+pila ends
+datos segment 'data'
+	msg db 'hola mundo$'
+datos ends
+codigo segment 'code'
+	assume cs:codigo, ds:datos, ss:pila
+	main PROC
+		mov ax,datos
+		mov ds,ax
+        
+		; Imprimir 7 veces la cadena
+		mov cx,0
+		bucle:
+			mov dx, OFFSET msg
+			mov ah,9
+			int 21h
+			; actualizar el contador y comprobar la condicion
+			inc cx
+			cmp cx,7
+			jne bucle
+		; Terminar y salir
+		mov ax,4C00h
+		int 21h
+	main ENDP
+codigo ends
+
+END main
