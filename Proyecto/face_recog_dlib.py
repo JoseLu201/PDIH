9edf07007f21eafd1a2f132a62bbdb6fa555ee01
joselu <joselu201@corre.ugr.es>
2023-05-08 17:05:20 +0200

    Practica Completada

--- Proyecto/face_recog_dlib.py
+++ Proyecto/face_recog_dlib.py
@@ -3,31 +3,24 @@
 import cv2
 import face_recognition as face
 
-
-frame = cv2.imread("cara.png")
+# Cargamos la cara la cual queremos RECONOCER
+frame = cv2.imread("done.png")
 
 framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+# Localizamos la cara de la imagen
 locs = face.face_locations(framergb)
 
 if locs is not None:
     for i in range(len(locs)):
         t,r,b,l = locs[i] # top, right, bottom ,left
-        lands = face.face_landmarks(framergb[t:b, l:r]) # diccionario de facciones de la cara
+        # Diccionario de facciones de la cara
+        lands = face.face_landmarks(framergb[t:b, l:r]) 
         cv2.rectangle(frame, (l,t), (r,b), (255,0,0), 2)
-        cv2.imshow("landma", frame)
-        cv2.waitKey() == ord(' ')
 
-
-
-# caras particulaes
-
-micara = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-locs = face.face_locations(micara)
-cod_micara = face.face_encodings(micara,locs,model='small')[0]
+cod_micara = face.face_encodings(framergb,locs,model='small')[0]
 
 #ahora la que tengo la codificacion de mi cara, en la camara uso lo siguiente
-#frame = camara
-#framergb
 print("abrimos cam\n")
 cap = cv2.VideoCapture(0)
 final = False
@@ -37,8 +30,8 @@ while not final:
     if ret:
         #mini = cv2.resize(framergb, dsize=(300,300),fx=0.25, fy=0.25)
         mini = framebgr
-        locs = face.face_locations(mini, model='hog') # esto o hog tmb es por defecto, cnn el mejor
-        cods = face.face_encodings(mini, locs, model='small') #podemos cambiar a small para obtener mejor rendimeinto pero peor calidad
+        locs = face.face_locations(mini, model='hog') # cnn o hog 
+        cods = face.face_encodings(mini, locs, model='small') #podemos cambiar de larga a small para obtener mejor rendimeinto pero peor calidad
         if locs is not None:
             for i in range(len(locs)):
                 if face.compare_faces(cod_micara, [cods[i]])[0]:
@@ -47,19 +40,13 @@ while not final:
                     color = (0,0,255)
                 t,r,b,l = locs[i] # top, right, bottom ,left
                 dist = face.face_distance(cod_micara,[cods[i]])[0]
-                #cv2.rectangle(framebgr, (l*4,t*4), (r*4,b*4), color, 2)
+                
                 cv2.rectangle(framebgr, (l,t), (r,b), color, 2)
+                cad = "Acuraccy " + str('%.3f'%dist)
+                img = cv2.cvtColor(framebgr, cv2.COLOR_BGR2RGB)
+                cv2.putText(framebgr, cad, (l,b+20), cv2.FONT_HERSHEY_TRIPLEX, 1, color)
         cv2.imshow("Video", framebgr)
         if cv2.waitKey(1) == ord(' '):
             final = True
     else:
         final = True
-
-
-##########
-# tambien podemos cargar modulos de dnn
-#"face_detector_20180220"
-#red = cv2.dnn.readNetFromTensorFlow("opencv_face_detector_uint8.pb")
-#blob = cv2.dnn.blobFromImage(imagen, 1.0, (300,300), [104., 117., 123.,], False, )
-#red.setInput(blob)
-#detection = red.ford
--- Proyecto/face_recog_simple.py
+++ Proyecto/face_recog_simple.py
@@ -4,6 +4,7 @@ import numpy as np
 #Instancia del clasificador de cascada con caras frontales
 cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
 
+#Abrimos la camara webcam
 cap = cv2.VideoCapture(0)
 if cap.isOpened():
     hframe = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
@@ -16,6 +17,9 @@ if cap.isOpened():
         ret, framebgr = cap.read()
         frame_gris = cv2.cvtColor(framebgr, cv2.COLOR_BGR2GRAY)
         if ret:
+            # Detectamos en la imagen lo que hayamos pedido.
+            # Esto nos devuelve la coordenada (x,y) de la 
+            # esquina superior izq y su ancho y largo
             res = cascade.detectMultiScale(frame_gris)
             for x,y,w,h in res:
                 cv2.rectangle(framebgr, (x,y), (x+w, y+h) ,(255,0,255),3)
@@ -23,8 +27,7 @@ if cap.isOpened():
             if cv2.waitKey(1) == ord(' '):
                 final = True
         else:
-            final = True
-            
+            final = True  
 else:
     print("No se pudo acceder a la cámara.")
 
